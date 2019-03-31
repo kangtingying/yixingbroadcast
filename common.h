311918b7e9f89bf27235c3a36bd938e2d4de6dfb
kangtingying <42210648+kangtingying@users.noreply.github.com>
2019-03-31 11:17:49 +0800

    Update common.h

--- common.h
+++ common.h
@@ -3,6 +3,7 @@
 //test2
 int initDB();
 int findById(int _id, int *_index, unsigned char *ucOut, int *iLen);
+//int insertToDB(unsigned char *pucRecvData);
 int insertToDB(unsigned char *pucRecvData);
 int anotherPacketIsExist(unsigned char *_pucRecvData);
 int getAnotherPacketData(unsigned char *_pucRecvData, unsigned char *pucOut, int *totalLen);
