6257ae0373d4eba86106e511b8b0c847e123c87c
kangtingying <42210648+kangtingying@users.noreply.github.com>
2019-03-31 11:25:11 +0800

    Update common.h

--- common.h
+++ common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 //test2
+//test3
 int initDB();
 int findById(int _id, int *_index, unsigned char *ucOut, int *iLen);
 //int insertToDB(unsigned char *pucRecvData);
